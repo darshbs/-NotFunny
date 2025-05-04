c7f37a120e7c8dc6478c8bd070a8a13c8aa3a489
Darshan <saidarshan.balaji@gmail.com>
2025-05-04 19:34:14 +0530

    test change

--- bot.py
+++ bot.py
@@ -1,11 +1,17 @@
 import discord
 
 class MyClient(discord.Client):
-  async def on_ready(self):
-    print('Logged on as {0}!'.format(self.user))
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello World!')
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents) 
-client.run('Your Token Here') # Replace with your own token. 
+client.run('') # Replace with your own token. 
